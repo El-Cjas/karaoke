0bcf02409e88e70c8d58f2f7590b51a525916722
0xCejas <elcejon85@gmail.com>
2026-01-21 12:02:58 -0400

    frfrr

--- hola.py
+++ hola.py
@@ -1,5 +1,10 @@
 import time
-from rich.console import Console
 
-console = Console()
+inicio = time.time()
 
+# Código que quieres medir
+time.sleep(1.6) # Pausa de 1.5 segundos
+
+fin = time.time()
+
+print(f"El proceso tardó {fin - inicio} segundos")
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from rich import print
 pygame.mixer.init()
 pygame.mixer.music.load("canciones\Ozuna x J Balvin x Chencho Corleone - Una Locura (Video Oficial) - Ozuna.mp3")
 pygame.mixer.music.play()
-pygame.mixer.music.set_pos(114)
+pygame.mixer.music.set_pos(0)
 
+tiempo_incio = time.time()
 
 
 letra = parsear_lrc("letras\Ozuna - Una Locura.es.lrc")
@@ -13,11 +14,10 @@ parte = letra[0]
 ##print(parte.values())
 num_elementos = len(letra)
 
-lista = {'tiempo': 209.3, 'texto': 'by RentAnAdviser.com'}
 
 
 for elemento in letra:
-    print(elemento)
-    for tiempo in elemento.values():
-        print(tiempo)
-        #print(f"minuto: {tiempo / 60}")
+    
+    while (time.time() - tiempo_incio) < elemento["tiempo"]:
+        time.sleep(0.09)
+    print(elemento["texto"])
